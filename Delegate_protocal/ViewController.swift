4f9fa58ba75d110d8984bb182c0b61ecdd163cf9
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-04-08 17:22:22 +0530

    create delegate

--- Delegate_protocal/ViewController.swift
+++ Delegate_protocal/ViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, productSelectionDelegate {
+    func didSelectProduct(name: String, imageName: String) {
+        productTitle.text = name
+        titleImageView.image = UIImage(named: imageName)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +20,23 @@ class ViewController: UIViewController {
         configureUI()
         // Do any additional setup after loading the view.
     }
-    
+    @objc func presentProductSelectionVC() {
+           let selectionSheet = productSelectVC()
+        selectionSheet.delegate = self
+        selectionSheet.modalPresentationStyle = .pageSheet
+        selectionSheet.sheetPresentationController?.detents = [.medium()]
+        selectionSheet.sheetPresentationController?.prefersGrabberVisible = true
+        present(selectionSheet, animated: true)
+        
+       }
     let titleImageView: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.contentMode = .scaleAspectFit
+        image.contentMode = .scaleAspectFill
         image.clipsToBounds = true
         image.image = UIImage(named: "appleDev")
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 8
         return image
     }()
     
@@ -42,6 +57,7 @@ class ViewController: UIViewController {
         button.configuration?.title = "choose product"
         button.configuration?.image = UIImage(systemName: "apple.logo")
         button.configuration?.imagePadding = 8
+        button.addTarget(self, action: #selector(presentProductSelectionVC), for: .touchUpInside)
         return button
     }()
     
@@ -52,14 +68,15 @@ class ViewController: UIViewController {
         
         titleImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
         titleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
-        titleImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        titleImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        titleImageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        
         
         productTitle.topAnchor.constraint(equalTo: titleImageView.bottomAnchor, constant: 20).isActive = true
         productTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         chooseButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        chooseButton.widthAnchor.constraint(equalToConstant: 181).isActive = true
+        chooseButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
         chooseButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
         chooseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
--- Delegate_protocal/productSelectVC.swift
+++ Delegate_protocal/productSelectVC.swift
@@ -6,30 +6,83 @@
 //
 
 import UIKit
+protocol productSelectionDelegate {
+    func didSelectProduct(name : String, imageName: String)
+}
 
 class productSelectVC: UIViewController {
 
+    var delegate : productSelectionDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
 
         // Do any additional setup after loading the view.
     }
+    @objc func tapIphoneButton(){
+        dismiss(animated: true)
+        delegate?.didSelectProduct(name: "IPHONE 15", imageName: "i15")
+    }
+    @objc func tapIpadButton(){
+        dismiss(animated: true)
+        delegate?.didSelectProduct(name: "Ipad Pro", imageName: "ipadPro")
+    }
+    @objc func tapMacButton(){
+        dismiss(animated: true)
+        delegate?.didSelectProduct(name: "MacBook pro", imageName: "macPro")
+    }
     let iPhoneButton:UIButton = {
         let button = UIButton()
+        button.configuration = .tinted()
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration?.title = "iPhone"
+        button.configuration?.image = UIImage(systemName: "iphone")
+        button.configuration?.imagePadding = 8
+       
         return button
     }()
     let iPadButton:UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = .tinted()
+        button.configuration?.title = "iPad"
+        button.configuration?.image = UIImage(systemName: "ipad")
+        button.configuration?.imagePadding = 8
         return button
     }()
     let macBookButton:UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = .tinted()
+        button.configuration?.title = "MacBook Pro"
+        button.configuration?.image = UIImage(systemName: "laptopcomputer")
+        button.configuration?.imagePadding = 8
         return button
     }()
     
+    private func configureUI(){
+        view.addSubview(iPhoneButton)
+        view.addSubview(iPadButton)
+        view.addSubview(macBookButton)
+        view.backgroundColor = .systemBackground
+        
+        iPadButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        iPadButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        iPadButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        iPadButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        iPhoneButton.bottomAnchor.constraint(equalTo: iPadButton.topAnchor, constant: -20).isActive = true
+        iPhoneButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        iPhoneButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        iPhoneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        macBookButton.topAnchor.constraint(equalTo: iPadButton.bottomAnchor, constant: 20).isActive = true
+        macBookButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        macBookButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        macBookButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        iPhoneButton.addTarget(self, action: #selector(tapIphoneButton), for: .touchUpInside)
+        iPadButton.addTarget(self, action: #selector(tapIpadButton), for: .touchUpInside)
+        macBookButton.addTarget(self, action: #selector(tapMacButton), for: .touchUpInside)
+    }
 
     /*
     // MARK: - Navigation
